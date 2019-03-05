2d4cd22c9a46b895674dd87da7689600cb874e02
SashaKor <akoroza@stuy.edu>
2019-03-04 23:16:39 -0500

    yay done

---
+++ 07_mongo/findrep.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+
+import sys
+
+
+def convert(name):
+    try:
+        f = open(name, "rU")
+        line = f.read()
+        line = str(line)
+        # print(line)
+        f.close()
+    except:
+        print("bad stuff happened")
+        return
+    ret = line.replace("{\"result\": {\"count\": \"37859\"}", "{\"result\": {\"count\": \"37859\"}}")
+    ret = ret.replace(", \"event\":", "}\n{\"event\":")
+    ret = ret[:-1]
+    print(ret)
+    # print(line.replace("{\"event\":", "\n{\"event\":"))
+    try:
+        f = open(name, "w")
+        f.write(ret)
+        f.close()
+    except:
+        print("error writing")
+
+def main():
+    convert(sys.argv[1])
+
+main()
--- 07_mongo/mongo.py
+++ 07_mongo/mongo.py
@@ -1,86 +1,96 @@
+#!/usr/bin/python3
+
 # Aaron Li, Aleksandra Koroza: Aaronoza
 # SoftDev2 pd8
 # K07 -- Import/Export Bank
 # 2019-02-29
+
 '''
-We used the English history dataset (found here https://github.com/jdorfman/awesome-json-datasets). The
-set is a collection of historical events. Each entry has an associated date, description, location, and associated literature.
+We used the English history dataset (found here http://www.vizgr.org/historical-events/search.php?format=json&begin_date=-3000000&end_date=20151231&lang=en). 
+The set is a collection of historical events. Each entry has an associated date, description, location, and associated literature.
 The collection was imported using the mongoimport command on our droplets.
 '''
+
+'''
+IMPORT INSTRUCTIONS:
+
+http://www.vizgr.org/historical-events/search.php?format=json&begin_date=-3000000&end_date=20151231&lang=en
+
+1) Click and download the link above to download the dataset, name it whatever you want with a .json file type.
+2) run $ python3 findrep.py <name_of_ur_file>.json to convert the file into a valid json format
+'''
+
 import pymongo
 
-#SERVER_ADDR = "69.55.59.139"
-SERVER_ADDR = "178.128.156.17"
+SERVER_ADDR = "69.55.59.139"
+# SERVER_ADDR = "178.128.156.17"
 connection = pymongo.MongoClient(SERVER_ADDR)
 db = connection.aaronoza
-col = db.history
+col = db.hw
 
-# Given a year, return relevant historical entries
-def yearDesc(year):
-    if type(year) == type(123):
-        cursor = col.find({"result.event.date": str(year)})
-        for entry in cursor:
-            print(entry)
-
-    else:
-        print("Please input a valid year!\n")
+# Given a year, return historical entries found given input:
+# an integer year (eg: 900)
+# a string date in yyyy/mm/dd format (eg: 2001/01/17)
+def yearAll(year):
+    cursor = col.find(
+        {
+            "event.date": str(year)
+        }
+    )
+    if cursor.count() == 0:
+        print("Sorry, this year wasn't found.\n")
+        return
+    [print(i, "\n") for i in cursor]
 
 # Given a specific date, return relevant historical entry
-def dateDesc(date):
-    if type(date) == type("abc"):
-        cursor = col.find({"result.event.date": str(year)})
-        for entry in cursor:
-            print(entry)
+def yearDesc(date):
+    if type(date) != type(12) or type(date) != type(""):
+        print("Please enter a valid date.\n")
+        return
 
-    else:
-        print("Please input a date with the format mm/dd/year \n")
+    cursor = col.find(
+        {
+            "event.date": str(date)
+        }
+    )
+    if cursor.count() == 0:
+        print("Sorry, this year wasn't found.")
+        return
+    print("\n----- Descriptions found for date:", date, "-----\n")
+    [print(i["event"]["description"], "\n") for i in cursor]
+    print("----- End of Descriptions found for date:", date, "-----\n")
 
-# Given a place (category 2) returns relevant historical entries
+# Given a place or topic (category 2) returns relevant historical entry descriptions
+# first letter of place should be capitalized
+# (eg: Americas, Asia, Religion, Arts and Sciences)
 def placeDesc(place):
     if type(place) == type("abc"):
-        cursor = col.find({"result.event.category2": place})
-        for entry in cursor:
-            print(entry)
-
-    else:
-        print("Please input a valid place! \n")
-
-# Return all entries in a given year and place
-def yearPlaceDesc(year,place):
-    if type(year) == type(123) and type(place)== type("abc"):
-        cursor = col.find({
-        "$and":[
-        {"result.event.date":str(year)},
-        {"result.event.category2":place},
-        ]
-        })
-        for entry in cursor:
-            print(entry)
-
-    else:
-        print("Please input a valid year and place! \n")
-
-
-#Given a word (or phrase), returns entries containing the word (or phrase).
-def phraseFind(phrase):
-    if type(phrase)== type("abc"):
-        col.create_index([("description", pymongo.TEXT )]) #text searching https://docs.mongodb.com/manual/text-search/
-        cursor = col.find({
-        "$text":
-        {"$search": phrase}
-        }
+        cursor = col.find(
+            {
+                "event.category2": place
+            }
         )
-        for entry in cursor:
-            print(entry)
+        [print(i["event"]["description"], "\n") for i in cursor]
+    else:
+        print("Please input a valid place.\n")
 
+# Given a word (or phrase), returns entries containing the word (or phrase)
+# and the date
+def find(phrase):
+    if type(phrase) == type("abc"):
+        cursor = col.find(
+            {
+                "event.lang": "en"
+            }
+        )
+        for i in cursor:
+            if phrase in i["event"]["description"]:
+                print("Date:", i["event"]["date"], "\n", i["event"]["description"], "\n")
     else:
         print("Please input another phrase to search! \n")
-'''
-testing
-'''
 
-#yearDesc(-292)
-#dateDesc()
-#placeDesc()
-#yearPlaceDesc(-300,"Seleucid Empire")
-phraseFind("Napoleonic")
+# test
+yearAll(100)
+yearDesc(100)
+placeDesc("Americas")
+find("valid")
