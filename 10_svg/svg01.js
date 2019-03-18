b8db97d9bd97f8d626aca58a45d94b6535901557
SashaKor <akoroza@stuy.edu>
2019-03-18 06:42:24 -0400

    10 done

--- 10_svg/svg01.js
+++ 10_svg/svg01.js
@@ -1,3 +1,10 @@
+// Aleksandra Koroza
+// SoftDev pd8
+//K #10: Ask Circles [Change || Die]
+// 2019-03-14
+
+// **issue remains with first circle clicked and producing a new circle
+
 var pic = document.getElementById('vimage'); //getting image
 var cl_button = document.getElementById('but_cl'); //tracking clear
 
@@ -5,33 +12,45 @@ var cl_button = document.getElementById('but_cl'); //tracking clear
 var changeColor= (e,c) =>{
   if (c.getAttribute('fill')== 'purple'){
     c.setAttribute('fill','blue');
+  }else{
+    c.setAttribute("fill", "purple");
+    c.setAttribute("cx", Math.floor(Math.random() * 400) + 20);
+    c.setAttribute("cy", Math.floor(Math.random() * 400) + 20);
+    e.stopPropagation();
   }
 }
 //func checks whether there are circles present in clicking location
-var circlePresent= (c) =>{
-  var children= pic.childNodes;
+//for some reason works after the first one
+var circlePresent= (x,y) =>{
+  var children= pic.children;
   var i;
   if(children.length != 0){
-    console.log(children.length);
-    console.log(children);
+    //console.log(children.length);
+    //console.log(children);
     for (i=0; i< children.length; i++){
-      var elem= children[i];
-      console.log(elem);
-      if((elem.getAttribute('cx') == c.getAttribute('cx')) && (elem.getAttribute('cy')== c.getAttribute('cy'))){
+      var child = children.item(i);
+      let cx = child.getAttribute("cx");
+      let cy = child.getAttribute("cy");
+      //console.log(elem);
+      if (Math.pow(cx - x, 2) + Math.pow(cy - y, 2) <= 400){
         return true;
       }
     }
     return false;
-  }
-  return false;
-}
-//func sets circle attributes amd draws if childNodes available
-var drawCircle = (i) => {
-  var circ = document.createElementNS('http://www.w3.org/2000/svg', 'circle'); //creating circles
-  circ.setAttribute("cx", i.offsetX); //offset between coor and padding edge
-  circ.setAttribute("cy", i.offsetY);
+}}
 
-  if (! circlePresent(circ)){
+//func sets circle attributes amd draws if no circles already present at location
+var drawCircle = (i) => {
+  i.stopPropagation();
+  i.preventDefault();
+  var x= i.offsetX;
+  var y= i.offsetY;
+
+  if (! circlePresent(x,y)){
+    console.log(circlePresent(x,y));
+    var circ = document.createElementNS('http://www.w3.org/2000/svg', 'circle'); //creating circles
+    circ.setAttribute("cx", i.offsetX); //offset between coor and padding edge
+    circ.setAttribute("cy", i.offsetY);
     circ.setAttribute("fill", "purple");
     circ.setAttribute("r", 20);
     //circ.addEventListener('click',changeColor(circ));
@@ -43,27 +62,10 @@ var drawCircle = (i) => {
 
 
 //func clears the circles by manipulating innerHTML and resetting x/y vals.
-//clearRect will not work since not canvas
+//can also loop through children and remove
 var clear = (e) => {
   pic.innerHTML='';
-  prev_x = NaN; //back to initial position
-  prev_y = NaN;
 };
-//you can kill off children!!
 
 pic.addEventListener('click', drawCircle); //image responsive to clicks
 cl_button.addEventListener('click', clear); //clear button responsive to clicks
-
-
-/*
-var pic= document.createElementNS(
-  "http://w3.org/200/svg","circle");
-  c.setAttribute("cx",0);
-  c.setAttribute("cy",0);
-  c.setAttribute("r",100);
-  c.setAttribute("fill","red");
-  c.setAttribute("stroke","black");
-
-  pic.appendChild(c);
-)
-*/
