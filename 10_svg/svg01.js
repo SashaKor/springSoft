92590d0f96065e58baad13c5a69e1c4cc1e287ee
SashaKor <akoroza@stuy.edu>
2019-03-17 20:55:10 -0400

    wip

--- 10_svg/svg01.js
+++ 10_svg/svg01.js
@@ -1,48 +1,46 @@
 var pic = document.getElementById('vimage'); //getting image
 var cl_button = document.getElementById('but_cl'); //tracking clear
 
-//var prev_x = NaN; //not applicable yet
-//var prev_y = NaN; // note that using null sets the values to 0,0; which is bad (line stretches to upper left)
-
-/*
-//func connects previous and current points
-var drawLine = (x,y) => {
-  if (isNaN(prev_x) && isNaN(prev_y)){
-    console.log('empty'); //means that image is reset, no need to draw
-    return;
+//func changes circle color/ facilitates random moving
+var changeColor= (e,c) =>{
+  if (c.getAttribute('fill')== 'purple'){
+    c.setAttribute('fill','blue');
   }
-  var children = pic.childNodes;
-  var line = document.createElementNS('http://www.w3.org/2000/svg', 'line'); //creates element with specified namespace URI
-  line.setAttribute('x1', prev_x); //careful abt capitalization!
-  line.setAttribute('x2', x);
-  line.setAttribute('y1', prev_y); // new attributes are added if did not exist
-  line.setAttribute('y2', y);
-  line.setAttribute('stroke', 'black');
-  pic.appendChild(line); //pic now has line with specified attributes
 }
-*/
+//func checks whether there are circles present in clicking location
+var circlePresent= (c) =>{
+  var children= pic.childNodes;
+  var i;
+  if(children.length != 0){
+    console.log(children.length);
+    console.log(children);
+    for (i=0; i< children.length; i++){
+      var elem= children[i];
+      console.log(elem);
+      if((elem.getAttribute('cx') == c.getAttribute('cx')) && (elem.getAttribute('cy')== c.getAttribute('cy'))){
+        return true;
+      }
+    }
+    return false;
+  }
+  return false;
+}
 //func sets circle attributes amd draws if childNodes available
 var drawCircle = (i) => {
   var circ = document.createElementNS('http://www.w3.org/2000/svg', 'circle'); //creating circles
   circ.setAttribute("cx", i.offsetX); //offset between coor and padding edge
   circ.setAttribute("cy", i.offsetY);
-  circ.setAttribute("fill", "purple");
-  //circ.setAttribute("stroke", "black"); //lines still overlap
-  circ.setAttribute("r", 20);
 
-  /*
-  for i in pic.childNodes:
-      if has same cx and cy
-      setattribute to gree
-  */
-  //if (pic.childNodes.length != 0){ //means that not reset, available to draw
-    //drawLine(i.offsetX, i.offsetY);
-  //}
+  if (! circlePresent(circ)){
+    circ.setAttribute("fill", "purple");
+    circ.setAttribute("r", 20);
+    //circ.addEventListener('click',changeColor(circ));
+    pic.appendChild(circ);
+    circ.addEventListener('click', function(e){
+      changeColor(e,circ);})
+    }
+  }
 
-  pic.appendChild(circ);
-  //prev_x = i.offsetX; //your curr now becomes your prev
-  //prev_y = i.offsetY;
-};
 
 //func clears the circles by manipulating innerHTML and resetting x/y vals.
 //clearRect will not work since not canvas
