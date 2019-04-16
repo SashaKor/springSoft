2ac50c9bc6c1ab48b5b5939a56b2f57d337d2a6e
Sasha <akoroza@stuy.edu>
2019-04-16 15:40:32 -0400

    furreal

--- 19_listcomp/new.py
+++ 19_listcomp/new.py
@@ -4,6 +4,7 @@ Shafali G, Aleksandra K., Maryann F.
 K #19: Ready, Set, Math!
 2019-04-16
 '''
+
 '''
 1 union of sets a,b  the union of [1,2,3] [2,3,4] == [1,2,3,4] no duplicates
 '''
@@ -22,16 +23,19 @@ def intersect(l1,l2):
 '''
 def set_diff(l1,l2):
     return  [x for x in l1 if x not in l2]
+
 '''
 4 symmetic difference of sets a,b  all members of a that aren't members of b and members of b that aren't in a.
 '''
 def symm_diff(l1,l2):
     return  [x for x in l1 if x not in l2] + [x for x in l2 if x not in l1]
+
 '''
 5 CARTESIAN PRoduct of a and b members are all possible ordered pairs (a,b) where a is a member of A and b is a member of B
 '''
 def carty_p(l1,l2):
     return  [(x,y) for x in l1 for y in l2]
+
 '''
 T
 E
